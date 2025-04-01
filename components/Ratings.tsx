a006c92daf4d0d8111755b5dd825f1a35c253647
Hanorah <ksdnorah@gmail.com>
2025-04-01 20:12:48 +0100

    first commit

--- components/Ratings.tsx
+++ components/Ratings.tsx
@@ -2,15 +2,17 @@ import Link from "next/link";
 
 export default function Ratings() {
 	return (
-		<div className="w-full flex justify-between sm:flex-col xm:flex-col gap-[20px]">
-			<div className="relative w-full h-0 pb-[56.25%] shadow-lg mt-6 mb-4 overflow-hidden rounded-2xl">
-				<iframe
-					loading="lazy"
-					className="absolute w-full h-full top-0 left-0 border-none"
-					src="https://www.canva.com/design/DAGi0gdgoKA/dZHKkgtB8enRkOh2g-J_0w/watch?embed"
-					allow="autoplay"
-				></iframe>
-
+		<div className="w-full flex justify-between sm:flex-col xm:flex-col gap-[20px] ">
+			<div className="relative w-full h-0 pb-[56.25%] shadow-lg mt-6 mb-4 overflow-hidden rounded-2xl bg-black">	<video
+				className="absolute w-full h-full top-0 left-0 border-none"
+				controls
+				autoPlay
+				muted
+				loop
+			>
+				<source src="/vid.mp4" type="video/mp4" />
+				Your browser does not support the video tag.
+			</video>
 			</div>
 		</div>
 	);
